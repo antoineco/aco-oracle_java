8faf3ef146e2faf529885443448922019ae7ee94
Antoine Cotten <tonio.cotten@gmail.com>
2015-07-18 03:30:59 +0200

    java 8u51, bump module version

--- manifests/javalist.pp
+++ manifests/javalist.pp
@@ -12,6 +12,7 @@ class oracle_java::javalist {
   case $oracle_java::maj_version {
     '8'     : {
       case $oracle_java::min_version {
+        '51'    : { $build = '-b16' }
         '45'    : { $build = '-b14' }
         '40'    : { $build = '-b25' }
         '31'    : { $build = '-b13' }
